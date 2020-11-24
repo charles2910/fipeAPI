a3fde911c0e1ccd49f6ffc4f274aaa1bab1cdf3a
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2020-11-24 12:23:29 -0300

    Initial code to fetch car makers from API

--- fipe.py
+++ fipe.py
@@ -1,3 +1,27 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import requests
+import json
+
 print('oi')
+
+marcas = requests.get('https://parallelum.com.br/fipe/api/v1/carros/marcas', headers={"user-agent": "curl/7.72.0"})
+
+maker = json.loads(marcas.text)
+
+
+for obj in maker:
+    print("[" + obj.codigo + "] " + obj.nome)
+
+selected_maker = input('Select the car maker by number: ')
+
+carros = requests.get('https://parallelum.com.br/fipe/api/v1/carros/marcas/'+selected_maker+'/modelos', headers={"user-agent": "curl/7.72.0"})
+
+cars = json.loads(carros.text)
+
+
+for obj in cars:
+    print("[" + obj.codigo + "] " + obj.nome)
+
+selected_car = input('Select the car by number: ')
+
